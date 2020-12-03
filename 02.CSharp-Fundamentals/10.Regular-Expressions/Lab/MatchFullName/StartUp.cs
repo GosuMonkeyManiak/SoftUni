665fa22c1701def7ff0b4118ff507efb83c354bd
Dimitar Kupanov <dimitarkupanov@gmail.com>
2020-12-03 21:17:41 +0200

    Regular Expressions Exercise

---
+++ 02.CSharp-Fundamentals/10.Regular-Expressions/Lab/MatchFullName/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchFullName
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            string text = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(text);
+
+            Console.WriteLine(string.Join(' ', matches));
+        }
+    }
+}
---
+++ 02.CSharp-Fundamentals/10.Regular-Expressions/Lab/MatchPhoneNumber/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchPhoneNumber
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"[+][3][5][9]([ ]|[-])[2]\1[0-9]{3}\1[0-9]{4}\b";
+
+            string text = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(text);
+
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
