894d508aa57a7efbce2960f4bf128c1da72b2d16
Dimitar Kupanov <dimitarkupanov@gmail.com>
2021-01-17 22:20:53 +0200

    MultiDimensional Arrays]

--- 04.CSharp-Advanced/02.Multidimensional-Arrays/Exercise/MatrixShuffling/MatrixShuffling.cs
+++ 04.CSharp-Advanced/02.Multidimensional-Arrays/Exercise/MatrixShuffling/MatrixShuffling.cs
@@ -32,24 +32,24 @@ namespace MatrixShuffling
             {
                 string[] tokens = Console.ReadLine()
                     .Split(" ", StringSplitOptions.RemoveEmptyEntries);
-                if (tokens[0].ToLower() == "end")
+                if (tokens[0] == "END")
                 {
                     break;
                 }
 
                 string command = tokens[0];
 
-                if (command.ToLower() == "swap")
+                if (command == "swap" && tokens.Length == 5)
                 {
                     int row1 = int.Parse(tokens[1]);
                     int col1 = int.Parse(tokens[2]);
                     int row2 = int.Parse(tokens[3]);
                     int col2 = int.Parse(tokens[4]);
 
-                    bool isInMatrix = (row1 >= 0 && row1 < rows) && (col1 >= 0 && col1 < cols) &&
-                                      (row2 >= 0 && row2 < rows) && (col2 >= 0 && col2 < cols);
+                    bool firstCoordinates = (row1 >= 0 && row1 < rows) && (col1 >= 0 && col1 < cols);
+                    bool secondCoordinates = (row2 >= 0 && row2 < rows) && (col2 >= 0 && col2 < cols);
 
-                    if (isInMatrix)
+                    if (firstCoordinates && secondCoordinates)
                     {
                         string temp = matrix[row1, col1];
                         matrix[row1, col1] = matrix[row2, col2];
---
+++ 04.CSharp-Advanced/02.Multidimensional-Arrays/Exercise/SnakeMoves/StartUp.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace SnakeMoves
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string word = Console.ReadLine();
+
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+
+            string[,] matrix = new string[rows, cols];
+
+            int count = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                if (row % 2 == 0)
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+                        if (count > word.Length - 1)
+                        {
+                            count = 0;
+                        }
+
+                        matrix[row, col] = word[count].ToString();
+
+                        count++;
+                    }
+                }
+                else
+                {
+                    for (int col = cols - 1; col >= 0; col--)
+                    {
+                        if (count > word.Length - 1)
+                        {
+                            count = 0;
+                        }
+
+                        matrix[row, col] = word[count].ToString();
+
+                        count++;
+                    }
+                }
+            }
+
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
