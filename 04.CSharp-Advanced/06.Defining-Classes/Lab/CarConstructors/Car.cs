2997fb9db2e58a35fc2fb415eb8dd175ba807fd3
Dimitar Kupanov <dimitarkupanov@gmail.com>
2021-01-30 21:42:02 +0200

    Intro in Classes/Defining simple Classes/Static Classes

---
+++ 04.CSharp-Advanced/06.Defining-Classes/Lab/CarConstructors/Car.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            fuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year) : this() 
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : this(make, model, year)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            if (fuelQuantity - distance * fuelConsumption > 0)
+            {
+                fuelQuantity -= distance * fuelConsumption;
+                return;
+            }
+
+            Console.WriteLine("Not enough fuel to perform this trip!");
+            return;
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {Make}\nModel: {Model}\nYear: {Year}\nFuel: {FuelQuantity:F2}L";
+        }
+    }
+}
---
+++ 04.CSharp-Advanced/06.Defining-Classes/Lab/CarEngineAndTires/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
