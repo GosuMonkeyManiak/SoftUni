e00e96296a18c3ff53ac52b3e1045c7d5f4783ba
Dimitar Kupanov <dimitarkupanov@gmail.com>
2021-02-08 21:56:17 +0200

    Generics Lab

---
+++ 04.CSharp-Advanced/09.Generics/Lab/BoxOfT/Box.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> stack;
+
+        public Box()
+        {
+            stack = new Stack<T>();
+        }
+
+        public int Count { get => stack.Count; }
+
+        public void Add(T element)
+        {
+            this.stack.Push(element);
+        }
+
+        public T Remove()
+        {
+            return stack.Pop();
+        }
+    }
+}
---
+++ 04.CSharp-Advanced/09.Generics/Lab/GenericScale/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> number = new EqualityScale<int>(100, 100);
+
+            Console.WriteLine(number.AreEqual());
+        }
+    }
+}
