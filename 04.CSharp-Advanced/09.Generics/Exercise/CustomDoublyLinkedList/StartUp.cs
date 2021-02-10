0a3de90e38d150477218f42c8fed3ee4ac1398d4
Dimitar Kupanov <dimitarkupanov@gmail.com>
2021-02-10 12:53:06 +0200

    Fix

---
+++ 04.CSharp-Advanced/09.Generics/Exercise/CustomDoublyLinkedList/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomDoublyLinkedList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
