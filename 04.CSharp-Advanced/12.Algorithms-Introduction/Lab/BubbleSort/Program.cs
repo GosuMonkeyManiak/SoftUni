4212ddcbbfcb08aad13fffd533b21b88c1182d3a
Dimitar Kupanov <dimitarkupanov@gmail.com>
2021-02-20 17:22:08 +0200

    Algorithms Intro

---
+++ 04.CSharp-Advanced/12.Algorithms-Introduction/Lab/BubbleSort/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 65, 25, 10, 2, 2, 4, 5 };
+
+            numbers = OptimazedBubbleSort(numbers);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        static int[] BubbleSort(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = 0; j < numbers.Length - 1 - i; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+
+            return numbers;
+        } //stable
+
+        static int[] OptimazedBubbleSort(int[] numbers)
+        {
+            bool swapped;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                swapped = false;
+
+                for (int j = 0; j < numbers.Length - 1 - i; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+
+            return numbers;
+        } //stable
+    }
+}
---
+++ 04.CSharp-Advanced/12.Algorithms-Introduction/Lab/SelectionSort/StartUp.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 4, 5, 3, 2, 4, 1 };
+
+            numbers = StableSelectionSort(numbers);
+
+            Console.WriteLine(string.Join(" ", numbers));
+
+            string[] text = { "paper", "true", "soap", "floppy", "flower" };
+
+            text = SelectionSortStrings(text);
+
+            Console.WriteLine(string.Join(" ", text));
+        }
+
+        static int[] SelectionSort(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                int minElementIndex = i;
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[minElementIndex] > numbers[j])
+                    {
+                        minElementIndex = j;
+                    }
+                }
+
+                if (minElementIndex != i)
+                {
+                    int element = numbers[i];
+                    numbers[i] = numbers[minElementIndex];
+                    numbers[minElementIndex] = element;
+                }
+            }
+
+            return numbers;
+        } //sort numbers
+
+        static string[] SelectionSortStrings(string[] text)
+        {
+            for (int i = 0; i < text.Length - 1; i++)
+            {
+                int minIndex = i;
+
+                for (int j = i + 1; j < text.Length; j++)
+                {
+                    if (text[minIndex].CompareTo(text[j]) > 0)
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                if (minIndex != i)
+                {
+                    string temp = text[i];
+                    text[i] = text[minIndex];
+                    text[minIndex] = temp;
+                }
+            }
+
+            return text;
+        } //sort text
+
+        static int[] StableSelectionSort(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                int minIndex = i;
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[minIndex] > numbers[j])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                int key = numbers[minIndex];
+
+                while (minIndex > i)
+                {
+                    numbers[minIndex] = numbers[minIndex - 1];
+                    minIndex--;
+                }
+
+                numbers[i] = key;
+            }
+
+            return numbers;
+        }
+    }
+}
