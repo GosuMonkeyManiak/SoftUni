33af4dadef22ff9bcf949a2867a7d98e6be03efd
Dimitar Kupanov <dimitarkupanov@gmail.com>
2021-02-20 17:21:30 +0200

    C#-Advanced Final Exam

---
+++ 04.CSharp-Advanced/13.Exam/Exam-20-Feb-2021/TheFightForGondor/StartUp.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheFightForGondor
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            int numbersOfWave = int.Parse(Console.ReadLine());
+
+            List<int> plates = new List<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            int index = 0;
+
+            Stack<int> orcs = new Stack<int>();
+
+            for (int i = 1; i <= numbersOfWave; i++)
+            {
+                orcs = new Stack<int>(Console.ReadLine()
+               .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse));
+
+                if (i % 3 == 0)
+                {
+                    int plate = int.Parse(Console.ReadLine());
+                    plates.Add(plate);
+                }
+
+
+                while (orcs.Count > 0 && plates.Count > 0)
+                {
+                    int plate = plates[index];
+                    int orc = orcs.Peek();
+
+                    if (plate == orc)
+                    {
+                        plates.RemoveAt(index);
+                        orcs.Pop();
+                    }
+                    else if (plate > orc)
+                    {
+                        orcs.Pop();
+                        plates[index] -= orc;
+
+                        if (plates[index] <= 0)
+                        {
+                            plates.RemoveAt(index);
+                        }
+                        //decrease a first plate
+                    }
+                    else if (orc > plate)
+                    {
+                        plates.RemoveAt(index);
+                        orc -= plate;
+
+                        if (orc <= 0)
+                        {
+                            orcs.Pop();
+                        }
+                        else
+                        {
+                            orcs.Pop();
+                            orcs.Push(orc);
+                        }
+
+                    }
+
+                }
+
+                if (plates.Count <= 0)
+                {
+                    break;
+                }
+            }
+
+            if (plates.Count > 0)
+            {
+                Console.WriteLine($"The people successfully repulsed the orc's attack.");
+            }
+            else
+            {
+                Console.WriteLine($"The orcs successfully destroyed the Gondor's defense.");
+            }
+
+            if (orcs.Count > 0)
+            {
+                Console.WriteLine($"Orcs left: {string.Join(", ", orcs)}");
+            }
+
+            if (plates.Count > 0)
+            {
+                Console.WriteLine($"Plates left: {string.Join(", ", plates)}");
+            }
+        }
+    }
+}
---
+++ 04.CSharp-Advanced/13.Exam/Exam-20-Feb-2021/TheRace/Race.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheRace
+{
+    public class Race
+    {
+
+        private List<Racer> data;
+
+        public Race(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new List<Racer>();
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count { get => data.Count; }
+
+        public void Add(Racer racer)
+        {
+            if (data.Count == Capacity)
+            {
+                return;
+            }
+
+            data.Add(racer);
+        }
+
+        public bool Remove(string name)
+        {
+            Racer racer = data.FirstOrDefault(x => x.Name == name);
+
+            if (racer != null)
+            {
+                data.Remove(racer);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Racer GetOldestRacer()
+        {
+            Racer racer = data
+                .OrderByDescending(x => x.Age)
+                .FirstOrDefault();
+
+            return racer;
+        }
+
+        public Racer GetRacer(string name)
+        {
+            return data.FirstOrDefault(x => x.Name == name);
+        }
+
+        public Racer GetFastestRacer()
+        {
+            return data.OrderByDescending(x => x.Car.Speed).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Racers participating at {Name}:");
+
+            foreach (var racer in data)
+            {
+                sb.AppendLine(racer.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
