143e093dc738b0eada61ac46bf90794e86520974
Dimitar Kupanov <dimitarkupanov@gmail.com>
2021-02-25 23:19:22 +0200

    last problem from inheritance

---
+++ 05.CSharp-OOP/01.Inheritance/Exercise/Animals/Animal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Gender { get; set; }
+
+        public virtual string ProduceSound()
+        {
+            return string.Empty;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{this.GetType().Name}");
+            sb.AppendLine($"{this.Name} {this.Age} {this.Gender}");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
--- 05.CSharp-OOP/01.Inheritance/Exercise/Animals/StartUp.cs
+++ 05.CSharp-OOP/01.Inheritance/Exercise/Animals/StartUp.cs
@@ -1,10 +1,63 @@
-﻿namespace Animals
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            List<Animal> animals = new List<Animal>();
+
+            while (true)
+            {
+                string typeOfAnimal = Console.ReadLine();
+                if (typeOfAnimal == "Beast!")
+                {
+                    break;
+                }
+                string[] info = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+
+                if (string.IsNullOrEmpty(info[0]) ||
+                    int.Parse(info[1]) < 0 ||
+                    string.IsNullOrEmpty(info[2]))
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+
+                switch (typeOfAnimal)
+                {
+                    case "cat":
+                        animals.Add(new Cat(info[0], int.Parse(info[1]), info[2]));
+                        break;
+
+                    case "dog":
+                        animals.Add(new Dog(info[0], int.Parse(info[1]), info[2]));
+                        break;
+
+                    case "frog":
+                        animals.Add(new Frog(info[0], int.Parse(info[1]), info[2]));
+                        break;
+
+                    case "kitten":
+                        animals.Add(new Kitten(info[0], int.Parse(info[1])));
+                        break;
+
+                    case "tomcat":
+                        animals.Add(new Tomcat(info[0], int.Parse(info[1])));
+                        break;
+                }
+            }
+
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+                animal.ProduceSound();
+            }
         }
     }
 }
