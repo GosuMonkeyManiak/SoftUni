7b4eb968d818340cf7205de76c92ee4c79f2b78a
Dimitar Kupanov <dimitarkupanov@gmail.com>
2021-10-30 11:43:23 +0300

    Update

--- 07.Entity-Framework-Core/03.Introduction-To-EF/SoftUni/Data/SoftUniContext.cs
+++ 07.Entity-Framework-Core/03.Introduction-To-EF/SoftUni/Data/SoftUniContext.cs
@@ -29,7 +29,7 @@ namespace SoftUni.Data
             {
                 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
 
-                optionsBuilder.UseSqlServer("Server=.;Database=SoftUni;User Id=sa;Password=Mitko875486123");
+                optionsBuilder.UseSqlServer("");
             }
         }
 
