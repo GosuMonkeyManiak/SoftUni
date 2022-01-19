d5c73d1dd40630fa31bcb0f1a1a325d24d297ef1
Dimitar Kupanov <dimitarkupanov@gmail.com>
2022-01-19 09:25:54 +0200

    Solve not complete exercise

---
+++ 01.CSharp-Programming-Basics/05.Simple-Operations-Exerceses/HomeWork/SimpleOperationsExercise/AlcoholMarket/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AlcoholMarket
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double whiskeyPrice = double.Parse(Console.ReadLine());
+
+            double beеrLiters = double.Parse(Console.ReadLine());
+            double wineLiters = double.Parse(Console.ReadLine());
+            double rakiqLiters = double.Parse(Console.ReadLine());
+            double whiskeyLiters = double.Parse(Console.ReadLine());
+
+            double rakiqPrice = whiskeyPrice / 2;
+            double winePrice = rakiqPrice - (rakiqPrice * 0.40);
+            double beerPrice = rakiqPrice - (rakiqPrice * 0.80);
+
+            double neededMoneyForAlcohol = (whiskeyLiters * whiskeyPrice)
+                                           + (rakiqLiters * rakiqPrice)
+                                           + (wineLiters * winePrice)
+                                           + (beеrLiters * beerPrice);
+
+            Console.WriteLine($"{neededMoneyForAlcohol:f2}");
+        }
+    }
+}
--- 01.CSharp-Programming-Basics/05.Simple-Operations-Exerceses/HomeWork/SimpleOperationsExercise/CharityCampaign/Program.cs
+++ 01.CSharp-Programming-Basics/05.Simple-Operations-Exerceses/HomeWork/SimpleOperationsExercise/CharityCampaign/Program.cs
@@ -6,7 +6,23 @@ namespace CharityCampaign
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int daysOfCharity = int.Parse(Console.ReadLine());
+
+            int numberOfConfectioner = int.Parse(Console.ReadLine());
+
+            int numberOfCakesPerDay = int.Parse(Console.ReadLine());
+            int numberOfWafflesPerDay = int.Parse(Console.ReadLine());
+            int numberOfPanCakePerDay = int.Parse(Console.ReadLine());
+
+            int allCakes = (numberOfConfectioner * numberOfCakesPerDay) * daysOfCharity;
+            int allWaffles = (numberOfConfectioner * numberOfWafflesPerDay) * daysOfCharity;
+            int allPancakes = (numberOfConfectioner * numberOfPanCakePerDay) * daysOfCharity;
+
+            decimal collectedMoney = (allCakes * 45.00M) + (allWaffles * 5.80M) + (allPancakes * 3.20M);
+
+            decimal moneyForCharity = collectedMoney - collectedMoney / 8;
+
+            Console.WriteLine($"{moneyForCharity:f2}");
         }
     }
 }
